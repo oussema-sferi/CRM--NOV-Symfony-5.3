34632eb1f2eb6bfb51149ea8f91f67e948e81041
Amessuo <oussema.s@itstacks.net>
2021-07-21 21:53:44 +0100

    login route protected

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,6 +14,9 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        if($this->getUser()) {
+            return $this->redirectToRoute('all_contacts');
+        }
         // if ($this->getUser()) {
         //     return $this->redirectToRoute('target_path');
         // }
